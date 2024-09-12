0af4d038920666f4677cbade43d2bcaab81fcbf3
ayabarakat3 <ayabarakat14@gmail.com>
2024-09-12 02:39:50 -0400

    Initialize inventory with product objects

---
+++ inventory_management.js
@@ -0,0 +1,11 @@
+// Task 1: Create an Inventory Array of Product Objects
+let inventory = 
+[{Name: "Mascara", price: 20, quantity: 100, lowStockLevel: 20},
+{Name: "Lipstick", price: 24, quantity: 150, lowStockLevel: 30},
+{Name: "Highlighter", price: 28, quantity: 90, lowStockLevel: 10},
+{Name: "Powder", price: 40, quantity: 130, lowStockLevel: 25},
+{Name: "Concealer", price: 35, quantity: 170, lowStockLevel: 40},];
+// An array was created to list 5 chosen products.
+// Products, price, quanity and low stock level numbers were decided. 
+console.log(inventory);
+
