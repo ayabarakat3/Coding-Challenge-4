a7906a21d8b5c9da3ad7e6c591b6618bc9680997
ayabarakat3 <ayabarakat14@gmail.com>
2024-09-12 14:09:15 -0400

    Initialize inventory with product objects

--- inventory_management.js
+++ inventory_management.js
@@ -1,12 +1,17 @@
 // Task 1: Create an Inventory Array of Product Objects
 
 const inventory = [
-{ name: 'Laptop', price: 1200, quantity: 10, lowStockLevel: 3 },
-{ name: 'Smartphone', price: 800, quantity: 5, lowStockLevel: 2 },
-{ name: 'Tablet', price: 400, quantity: 7, lowStockLevel: 1 },
-{ name: 'Headphones', price: 100, quantity: 15, lowStockLevel: 5 },
-{ name: 'Smartwatch', price: 250, quantity: 3, lowStockLevel: 1 }];
+{ name: 'Lipstick', price: 24, quantity: 400, lowStockLevel: 30 },
+{ name: 'Mascara', price: 25, quantity: 500, lowStockLevel: 35 },
+{ name: 'Blush', price: 28, quantity: 200, lowStockLevel: 20},
+{ name: 'Highlighter', price: 28, quantity: 150, lowStockLevel: 15 },
+{ name: 'Bronzer', price: 40, quantity: 300, lowStockLevel: 30 }];
 // An array was created to list 5 given products.
 // Products, price, quanity and low stock level numbers were given from the sample data. 
 console.log(inventory);
 // Task 1 is now complete.
+
+//Task 2: Create a Function to Display Product Details
+function displayProductDetails(inventory) {
+let stockStatus = product.quantity <= product.lowStockLevel ? 'Low Stock' : 'In Stock';
+}
