3a274223b16caf1a90244e5d1fcc6236a295c64d
ayabarakat3 <ayabarakat14@gmail.com>
2024-09-15 21:09:22 -0400

    Create processSale function

--- inventory_management.js
+++ inventory_management.js
@@ -70,4 +70,5 @@ processSale('Duck', 70);
 // Error message tested on items not found in the inventory.
 // Error message displays expected output. 
 //Task 6 is now complete. 
-// All tasks have now been complete.
+
+// All tasks have now been complete. 
